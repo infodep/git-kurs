f16b7cfa9893088d5c7d1f3467b3fd42295662e9
Henrik T. Kaarbø <henrtk@stud.ntnu.no>
2023-10-03 19:18:33 +0200

    Lag oppgave X :)

--- python/oppgave X.py
+++ python/oppgave X.py
@@ -1,52 +1,119 @@
 import numpy as np
 import matplotlib.pyplot as plt
- 
-def EulerCromer(tmax, x0, y0, v0, u0, m, tau):
-    # tmax er tiden jorden bruker rundt solen
-    # x0 og y0 er startbetingelser for jordens posisjon
-    # v0 og u0 er starbetingelser for farten til jorden
-    # m er massen til jorden og tau er steglengden.
- 
-    N = int(round(tmax/tau))   #np.zeros(N) lager en liste bestående av bare 0ere av lengde N
-    x = np.zeros(N)
-    y = np.zeros(N)
-    u = np.zeros(N)
-    v = np.zeros(N)
-    radiuser = np.zeros(N)
- 
-    # startbetingelser
-    u[0] = u0
-    v[0] = v0
-    x[0] = x0
-    y[0] = y0
-    radiuser[0] = np.sqrt((x[0]) ** 2 + (y[0]) ** 2)
- 
-    for n in range(1, N):
-        u[n] = u[n - 1] - 4 * np.pi ** 2 * x[n - 1] * tau / (radiuser[n - 1] ** 3)
-        v[n] = v[n - 1] - 4 * np.pi ** 2 * y[n - 1] * tau / (radiuser[n - 1] ** 3)
-        x[n] = x[n - 1] + u[n] * tau
-        y[n] = y[n - 1] + v[n] * tau
-        radiuser[n] = np.sqrt((x[n]) ** 2 + (y[n]) ** 2)
- 
- 
-    return x, y  # posisjons- og farts-lister
- 
-# startbetingelser:
-x0 = 1    # Tenk deg at solen er i origo og at jorden starter i posisjon(1,0)
-y0 = 0
-u0 = 0    # startfarten i x-retning er 0
-v0 = 2*3.1415623   # startfarten i y-retning er 2*pi
-m =  1 / 333480    # dette er massen til Jorden i forhold til massen til Solen
-tmax = 1           # Omløpstiden rundt Solen er 1(år)
-tau = 0.01         # denne skrittlengden er såpass liten at plottet blir fint nok
- 
-x1, y1 = EulerCromer(tmax, x0, y0, v0, u0, m, tau)
- 
-# Plotter banen til planeten rundt sola
-plt.figure()
-plt.plot(x1, y1)
-circle = plt.Circle((0, 0), radius=0.06, fc='yellow')
-plt.gca().add_patch(circle)
-plt.xlabel(r'x [AU]')
-plt.ylabel(r'y [AU]')
-plt.show()
+from typing import Tuple, Callable
+
+# Oppgaven er nederst!
+
+# Gi noen typehinting for funksjonen, slik at du enklere kan se hva den returnerer
+posisjon, fart, tid = np.ndarray, np.ndarray, np.ndarray 
+
+def løsODE(tmax, x0: np.ndarray, v0: np.ndarray, dt, ODE: Callable, showPlot=True) -> Tuple[posisjon, fart, tid]:
+    """
+    Løser en andre ordens ODE med Eulers metode. 
+
+    Parametere
+    ----------
+    tmax : float
+        Tidslengden som skal løses over.
+    x0 : np.ndarray
+        Startposisjonen.
+    v0 : np.ndarray
+        Startfarten.
+    dt : float
+        Tidssteget.
+    ODE : function
+        Funksjonen som skal løses. Denne må ta inn x, v og t som parametere.
+    showPlot : bool, optional
+        Viser plottet av løsningen hvis True. Default er True.
+    """
+
+    ts = np.arange(0, tmax, dt)
+    xs = np.zeros((len(ts), len(x0)))   
+    vs = np.zeros((len(ts), len(v0)))
+
+    xs[0] = x0
+    vs[0] = v0
+
+    for i in range(1, len(ts)):
+        xs[i] = xs[i-1] + vs[i-1] * dt
+        vs[i] = vs[i-1] + ODE(xs[i-1], vs[i-1], ts[i-1]) * dt
+    
+    if showPlot:
+        plt.plot(xs[:, 0], xs[:, 1])
+        plt.show()
+    
+    return xs, vs, ts
+
+
+def plotXY(xy, vs, lagre=False):
+    """
+    Lager et plot av banen til et objekt i to dimensjoner.
+    Fargen til banen i et punkt gir hastigheten til objektet!
+    """
+
+    # Denne bare plotter for special case 2 dimesnjoner, ikke tenk så mye på det!
+    
+    from matplotlib.collections import LineCollection
+    
+    points = np.array([xy[:,0], xy[:,1]]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+    fig, axs = plt.subplots()
+
+    # Create a continuous norm to map from data points to colors
+    norm = plt.Normalize(np.linalg.norm(vs, axis = 1).min(), np.linalg.norm(vs, axis = 1).max())
+    lc = LineCollection(segments, cmap='viridis', norm=norm)
+    lc.set_array(np.linalg.norm(vs, axis = 1))
+    lc.set_linewidth(2)
+    line = axs.add_collection(lc)
+    axs.plot(xy[:,0], xy[:,1])
+    fig.colorbar(line, ax=axs, label="Hastighet")
+    plt.show()
+    if lagre:
+        plt.savefig("oppgaveX.png")
+
+
+def lagreLøsningIBilde(xs, vs, ts):
+    fig, ax = plt.subplots(1,2)
+
+    ax[0].set_title("Fart")
+    ax[1].set_title("Posisjon")
+
+    for dim in range(len(xs[0])):
+        ax[1].plot(ts, xs[:, dim])
+        ax[0].plot(ts, vs[:, dim])
+
+    plt.savefig("oppgaveX.png")
+
+def minDifferensialligning(x, v, t):
+    # Dempet pendel i to dimensjoner! 
+    return -x -0.1*v #+ #0.1*np.sin(t)
+
+
+def dinDifferensialligning(x: np.ndarray, v, t):
+    """
+    Nå er det din tur, lag din egen differensialligning som tar inn x, v og t som parametere og returnerer en numpy-array med
+    akselerasjonen. Denne arrayen må ha dimensjon lik v og x sin dimensjon, ie. hvis x = [x1, x2] må a = [a1, a2]
+    """
+    return x.copy()*0
+
+# Skriv din kode under! 
+# Du kan begynne med å kjøre
+# --------
+# position, velocity, times = løsODE(10, np.array([0, 0]), np.array([1, 1]), 0.01, minDifferensialligning)
+# plotXY(position, velocity)
+# --------
+# ^ for å se hvordan løsODE fungerer.
+# --------
+# interessant = bool(input("Er dette interessant? (1 hvis ja, 0 hvis nei)"))
+#
+# if interessant:
+#     plotXY(position, velocity, lagre=True)
+# else:
+#     print("Ok, ha en fin dag!")
+#
+# Se så på funksjonen LøsODE og se om du kan lage en egen differensialligning som gir et gøyt plot :)
+# Last så opp KUN din funksjon på GitHub vha git med et passende navn under og send en pull request!
+# Legg gjerne ved det lagrede bildet ditt i pull-requesten  under en mappe med navn "Bilder" =)
+# -- Henrik, differensialligning-entusiast
+
