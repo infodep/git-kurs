7ad90c30451f50a9f87d2cf8a762d4914e49d6ba
Trond Hauklien <36609741+trondhauklien@users.noreply.github.com>
2024-01-29 12:31:40 +0100

    Remove binary encoding of string

--- python/script.py
+++ python/script.py
@@ -3,7 +3,7 @@ import hashlib
 Run this file to print the flag.
 """
 def main():
-    print(f"GIT_KURS_{hashlib.sha256(b'Git er ganske kult!').hexdigest()[:8]}")
+    print(f"GIT_KURS_{hashlib.sha256('Git er ganske kult!').hexdigest()[:8]}")
 
 if __name__ == "__main__":
     main()
