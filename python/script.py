81bf57735acc08cf87cbdba9aef5bf1899432962
Trond Hauklien <36609741+trondhauklien@users.noreply.github.com>
2024-01-29 12:35:37 +0100

    Gave up and made the flag completely random

--- python/script.py
+++ python/script.py
@@ -1,9 +1,10 @@
-import hashlib
+import random
+import string
 """
 Run this file to print the flag.
 """
 def main():
-    print(f"GIT_KURS_{hashlib.sha256(b'Git er ganske kult!')[:8]}")
+    print(f"GIT_KURS_{''.join(random.choice(string.ascii_letters) for _ in range(8))}")
 
 if __name__ == "__main__":
     main()
